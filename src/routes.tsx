bf2e2358c28c4d45b54ce7e5f7c85c5748e79176
eliseucbrito <ecb2@cin.ufpe.br>
2025-03-20 16:49:20 -0300

    feat: refactor routes for better visualization and add PublicProfessor routes

--- src/routes.tsx
+++ src/routes.tsx
@@ -1,22 +1,28 @@
-import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import { ForbiddenPage } from '@cincoders/cinnamon';
 import { useAuth } from 'react-oidc-context';
-import apiBack from './services/api';
-import { Links, Roles } from './types/enums';
-import Home from './pages/Home/index';
-import PublicationsPage from './pages/Publications/Publications';
-import ProfessorPublicationsPage from './pages/Publications/ProfessorPublications';
-import StudentsPage from './pages/Students/Students';
-import ProfessorStudentsPage from './pages/Students/ProfessorStudents';
-import ProjectsPage from './pages/Projects/Projects';
-import ProfessorProjectsPage from './pages/Projects/ProfessorProjects';
-import ProfessorsListPage from './pages/ProfessorsList';
-import QualisPage from './pages/Qualis';
-import ImportXml from './pages/ImportXml';
-import ProfessorInfo from './pages/Professor';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import PageCin from './components/PageCin';
+import LinksList from './components/PublicProfessor/Links/LinksList';
+import PatentsList from './components/PublicProfessor/Patents/PatentsList';
+import ProjectsList from './components/PublicProfessor/Projects/ProjectsList';
+import PublicationList from './components/PublicProfessor/Publications/PublicationsList';
+import SupervisionsList from './components/PublicProfessor/Supervisions/SupervisionsList';
+import Home from './pages/Home/index';
+import ImportXml from './pages/ImportXml';
 import PatentsPage from './pages/Patents/Patents';
 import ProfessorPatentsPage from './pages/Patents/ProfessorPatents';
+import ProfessorInfo from './pages/Professor';
+import ProfessorsListPage from './pages/ProfessorsList';
+import ProfessorProjectsPage from './pages/Projects/ProfessorProjects';
+import ProjectsPage from './pages/Projects/Projects';
+import ProfessorPublicationsPage from './pages/Publications/ProfessorPublications';
+import PublicationsPage from './pages/Publications/Publications';
+import PublicProfessor from './pages/PublicProfessor';
+import QualisPage from './pages/Qualis';
+import ProfessorStudentsPage from './pages/Students/ProfessorStudents';
+import StudentsPage from './pages/Students/Students';
+import apiBack from './services/api';
+import { Links, Roles } from './types/enums';
 
 function RoutesApp() {
   const auth = useAuth();
@@ -28,118 +34,33 @@ function RoutesApp() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route
-          path={Links.FORBIDDEN}
-          element={
-            <PageCin auth={auth} permittedRoles={['*']}>
-              <ForbiddenPage auth={auth} />
-            </PageCin>
-          }
-        />
-        <Route
-          path={Links.HOME}
-          element={
-            <PageCin auth={auth} permittedRoles={[Roles.USERS]}>
-              <Home />
-            </PageCin>
-          }
-        />
-        <Route
-          path={Links.IMPORT_XML}
-          element={
-            <PageCin auth={auth} permittedRoles={[Roles.USERS]}>
-              <ImportXml />
-            </PageCin>
-          }
-        />
-        <Route
-          path={Links.PUBLICATIONS}
-          element={
-            <PageCin auth={auth} permittedRoles={[Roles.USERS]}>
-              <PublicationsPage />
-            </PageCin>
-          }
-        />
-        <Route
-          path={Links.PROFESSOR_PUBLICATIONS}
-          element={
-            <PageCin auth={auth} permittedRoles={[Roles.USERS]}>
-              <ProfessorPublicationsPage />
-            </PageCin>
-          }
-        />
-        <Route
-          path={Links.PROFESSORS_LIST}
-          element={
-            <PageCin auth={auth} permittedRoles={[Roles.USERS]}>
-              <ProfessorsListPage />
-            </PageCin>
-          }
-        />
-        <Route
-          path={Links.PROJECTS}
-          element={
-            <PageCin auth={auth} permittedRoles={[Roles.USERS]}>
-              <ProjectsPage />
-            </PageCin>
-          }
-        />
-        <Route
-          path={Links.PROFESSOR_PROJECTS}
-          element={
-            <PageCin auth={auth} permittedRoles={[Roles.USERS]}>
-              <ProfessorProjectsPage />
-            </PageCin>
-          }
-        />
-        <Route
-          path={Links.PATENTS}
-          element={
-            <PageCin auth={auth} permittedRoles={[Roles.USERS]}>
-              <PatentsPage />
-            </PageCin>
-          }
-        />
-        <Route
-          path={Links.PROFESSOR_PATENTS}
-          element={
-            <PageCin auth={auth} permittedRoles={[Roles.USERS]}>
-              <ProfessorPatentsPage />
-            </PageCin>
-          }
-        />
-        <Route
-          path={Links.STUDENTS}
-          element={
-            <PageCin auth={auth} permittedRoles={[Roles.USERS]}>
-              <StudentsPage />
-            </PageCin>
-          }
-        />
-        <Route
-          path={Links.PROFESSOR_STUDENTS}
-          element={
-            <PageCin auth={auth} permittedRoles={[Roles.USERS]}>
-              <ProfessorStudentsPage />
-            </PageCin>
-          }
-        />
-        <Route
-          path={Links.QUALIS}
-          element={
-            <PageCin auth={auth} permittedRoles={[Roles.USERS]}>
-              <QualisPage />
-            </PageCin>
-          }
-        />
-        <Route
-          path={Links.PROFESSOR_INFO}
-          element={
-            <PageCin auth={auth} permittedRoles={[Roles.USERS]}>
-              <ProfessorInfo />
-            </PageCin>
-          }
-        />
+        <Route element={<PageCin auth={auth} permittedRoles={['*']} />}>
+          <Route path={Links.FORBIDDEN} element={<ForbiddenPage auth={auth} />} />
+
+          <Route element={<PublicProfessor />}>
+            <Route path={Links.PROFESSOR_PUBLICATIONS} element={<PublicationList />} />
+            <Route path={Links.PUBLIC_PROFESSOR_PROJECTS} element={<ProjectsList />} />
+            <Route path={Links.PUBLIC_PROFESSOR_SUPERVISIONS} element={<SupervisionsList />} />
+            <Route path={Links.PUBLIC_PROFESSOR_PATENTS} element={<PatentsList />} />
+            <Route path={Links.PUBLIC_PROFESSOR_LINKS} element={<LinksList />} />
+          </Route>
+        </Route>
+
+        <Route element={<PageCin auth={auth} permittedRoles={[Roles.USERS]} />}>
+          <Route path={Links.HOME} element={<Home />} />
+          <Route path={Links.IMPORT_XML} element={<ImportXml />} />
+          <Route path={Links.PUBLICATIONS} element={<PublicationsPage />} />
+          <Route path={Links.PROFESSOR_PUBLICATIONS} element={<ProfessorPublicationsPage />} />
+          <Route path={Links.PROFESSORS_LIST} element={<ProfessorsListPage />} />
+          <Route path={Links.PROJECTS} element={<ProjectsPage />} />
+          <Route path={Links.PROFESSOR_PROJECTS} element={<ProfessorProjectsPage />} />
+          <Route path={Links.PATENTS} element={<PatentsPage />} />
+          <Route path={Links.PROFESSOR_PATENTS} element={<ProfessorPatentsPage />} />
+          <Route path={Links.STUDENTS} element={<StudentsPage />} />
+          <Route path={Links.PROFESSOR_STUDENTS} element={<ProfessorStudentsPage />} />
+          <Route path={Links.QUALIS} element={<QualisPage />} />
+          <Route path={Links.PROFESSOR_INFO} element={<ProfessorInfo />} />
+        </Route>
       </Routes>
     </BrowserRouter>
   );
