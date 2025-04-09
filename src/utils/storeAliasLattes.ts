c9385e8e94d0352203a019b7796025f6b31c61c6
eliseucbrito <ecb2@cin.ufpe.br>
2025-04-09 10:29:27 -0300

    hotfix(PublicProfessor): move lattes code from localStorage to OutletContext to avoid production bug

--- src/utils/storeAliasLattes.ts
+++
@@ -1,5 +0,0 @@
-export const storeAliasLattes = (key: string, value?: string) => {
-  localStorage.setItem(key, value ?? '');
-};
-
-export const getAliasLattes = (key: string): string | null => localStorage.getItem(key);
