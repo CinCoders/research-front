e6901bb17e572f842bd68dc9d3914115b679e9b3
eliseucbrito <ecb2@cin.ufpe.br>
2025-04-22 23:34:52 -0300

    refactor: move GenericList StateContainer to a shared component

--- src/components/PublicProfessor/GenericList/GenericList.tsx
+++ src/components/PublicProfessor/GenericList/GenericList.tsx
@@ -1,32 +1,6 @@
 import { Box, Typography } from '@mui/material';
-import React, { ReactNode } from 'react';
-
-function StateContainer({ children, message }: { children?: ReactNode; message?: string }) {
-  return (
-    <Box
-      width='100%'
-      height='100'
-      display='flex'
-      flexDirection='column'
-      justifyContent='center'
-      alignItems='center'
-      gap={2}
-      style={{ marginTop: 300 }}
-    >
-      {children}
-      {message && (
-        <Typography variant='subtitle2' fontSize={20}>
-          {message}
-        </Typography>
-      )}
-    </Box>
-  );
-}
-
-StateContainer.defaultProps = {
-  children: null,
-  message: '',
-};
+import React from 'react';
+import StateContainer from '../../share/StateContainer';
 
 export interface GenericListProps<T> {
   data: T[] | null;
@@ -37,7 +11,7 @@ export default function GenericList<T>({ Card, data }: GenericListProps<T>) {
   if (!data || data.length === 0) {
     return (
       <StateContainer message='Nenhum dado encontrado'>
-        <Typography variant='h5' fontSize={20}>
+        <Typography variant='h5' fontSize={20} align='center'>
           Ocorreu um erro durante a busca. Tente novamente mais tarde.
         </Typography>
       </StateContainer>
